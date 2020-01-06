55551a661f921e0470d53137e26c4ce341f23f1d
lolicon <lolicon@mail.az>
2020-01-06 15:47:16 +0800

    init repo

---
+++ src/suspend.ts
@@ -0,0 +1,28 @@
+interface Cont<R> {
+  fail?: (_: any) => void
+  success?: (_: R) => void
+}
+
+interface Suspendable<O> {
+  (option: O): any
+}
+
+let suspend = <O extends Cont<any>>(src: Suspendable<O>) => async (opt: Omit<O, "success" | "fail">) => await new Promise<Parameters<Exclude<O["success"], undefined>>[0]>((resolve, _) => src({
+  ...opt,
+  success: right => resolve(right),
+  fail: (_) => resolve(undefined)
+} as O))
+
+export let login = () => suspend(wx.login)({})
+export let getWeRunData = () => suspend(wx.getWeRunData)({})
+export let checkSession = () => suspend(wx.checkSession)({})
+export let openSetting = () => suspend(wx.openSetting)({})
+export let getUserInfo = () => suspend(wx.getUserInfo)({})
+export let getBatteryInfo = () => suspend(wx.getBatteryInfo)({})
+export let getLocation = () => suspend(wx.getLocation)({})
+export let getSetting = () => suspend(wx.getSetting)({})
+
+export let request = suspend(wx.request)
+export let authorize = suspend(wx.authorize)
+
+
